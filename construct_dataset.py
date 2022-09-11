5c8cedfbacb7866d2ab521b6f0497b0b86eca8ea
Su Direkci <sudirekci@gmail.com>
2022-09-11 14:38:24 -0700

    3 params training

--- construct_dataset.py
+++ construct_dataset.py
@@ -7,6 +7,8 @@ training_wg = wd.WaveformGenerator(dataset_len=100000, add_glitch=False, add_noi
                                    directory=directory, path_to_glitschen=path_to_glitschen, svd_no_basis_coeffs=100)
 training_wg.construct_signal_dataset(perform_svd=True, save=True, filename='training_data')
 
+print('training data saved')
+
 # validation data
 validation_wg = wd.WaveformGenerator(dataset_len=10000, add_glitch=False, add_noise=True,
                                      directory=directory, path_to_glitschen=path_to_glitschen, svd_no_basis_coeffs=100)
@@ -16,6 +18,10 @@ validation_wg.calculate_dataset_statistics()
 
 validation_wg.save_data('validation_data')
 
+print('validation data saved')
+
 testing_wg = wd.WaveformGenerator(dataset_len=10000, add_glitch=False, add_noise=True,
                                   directory=directory, path_to_glitschen=path_to_glitschen, svd_no_basis_coeffs=100)
 testing_wg.construct_signal_dataset(perform_svd=False, save=True, filename='testing_data')
+
+print('testing data saved')
