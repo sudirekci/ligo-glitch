f391b4a613a643a1f1ae4183616890302d708a80
Su Direkci <sudirekci@gmail.com>
2022-09-25 17:46:34 -0700

    3 param extrinsic training

--- gwpe.py
+++ gwpe.py
@@ -25,13 +25,13 @@ from scipy.stats import multivariate_normal
 
 """
 python gwpe.py train new nde \
-    --data_dir /home/su.direkci/glitch_project/dataset_no_glitch_3p_svd_10_extrinsic/ \
-    --model_dir /home/su.direkci/glitch_project/models_no_glitch_w_noise/3d_22/ \
+    --data_dir /home/su.direkci/glitch_project/dataset_no_glitch_3p_svd_4_extrinsic/ \
+    --model_dir /home/su.direkci/glitch_project/models_no_glitch_w_noise/3d_23/ \
     --nbins 2 \
     --num_transform_blocks 1 \
     --nflows 3 \
     --batch_norm \
-    --lr 0.0002 \
+    --lr 0.0008 \
     --epochs 5 \
     --hidden_dims 32 \
     --activation elu \
@@ -212,7 +212,7 @@ class PosteriorModel(object):
         if not existing:
             # input_dim = self.wfd.nparams !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             # context_dim = self.wfd.context_dim !!!!!!!!!!!!!!!!!!!!!!!!!!!
-            context_dim = 40
+            context_dim = 16
             input_dim = 3
 
             self.model = model_creator(input_dim=input_dim,
