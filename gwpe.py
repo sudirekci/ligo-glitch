11cf64e7bb25631999248370f058afc32ca825c7
Su Direkci <sudirekci@gmail.com>
2022-10-09 12:25:20 -0700

    fix plot percentiles

--- gwpe.py
+++ gwpe.py
@@ -670,7 +670,7 @@ class PosteriorModel(object):
                 fig = corner.corner(params_samples[:, slice], truths=params_true[slice],
                                     labels=parameter_labels[slice], range=range1, density=True,
                                     hist_kwargs={"density":True}, bins=25,
-                                    hist2d_kwargs={"plot_contours":True})
+                                   levels=[68.2, 95.4, 99.7])
 
                 axes = fig.get_axes()
 
